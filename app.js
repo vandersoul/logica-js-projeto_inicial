96bf25b99590be15f7a55a8cfe007d5976bb990b
vandersoul <111820309+vandersoul@users.noreply.github.com>
2024-04-01 08:04:06 -0300

    kkk

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-alert("Bem vindo ao jogo do numero secreto!");
+let numeroSecreto = Math.floor(Math.random() * 5 + 1); // serve para declarar a variavel
+let tentativas = 1;
 
-let numeroSecreto = 3;
-let numeroDigitado = parseInt(prompt('Digite um numero de 0 a 15...'));
-
-if(numeroDigitado === numeroSecreto){ 
-    alert('parabens Voce acertou!');
-} else if(numeroDigitado<numeroSecreto) {
-    alert('Voce errou, o numeroSecreto e maior');
-} else {
-
-    alert('Voce errou, o numeroSecreto é menor');    
+// wile cria um laço de repetição enquanto a condição seja verdadeira
+while (tentativas < 4) {
+    let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+    // if é para verificar se o numero secreto é igual ao numero digitado
+   
+    if(numeroDigitado === numeroSecreto){
+        alert('Parabéns, você acertou!');
+        break;
+        // else if , é criar uma nova condição apos a condição anterior ser falsa
+    } else if(numeroDigitado > numeroSecreto){ 
+        alert('Você digitou um número maior que o número secreto');
+    } else {
+        alert('Você digitou um número menor que o número secreto');
+    }
+    tentativas++;
+    if(tentativas === 4){
+        alert(`Suas tentativas acabaram`);
+    } else if (tentativas <3){
+        alert(`você ainda tem  ${4 - tentativas} tentativas`);   
+    }else{
+        alert(`você ainda tem  ${4 - tentativas} tentativa`);
+    }
+    
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+if(tentativas < 4){
+    alert(`Você acertou com ${tentativas} ${palavraTentativa}`)
+}
+ 
---
+++ dados.js
@@ -0,0 +1,8 @@
+let idade = parseInt(prompt("qual sua idade?"));
+if (idade >= 18){
+    alert ("voce é maior de idade");
+    
+}   else{ 
+    alert ("voce é menor de idade");
+}
+
