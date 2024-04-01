981745bfae68084ad2a8d23accdf3eb1dc8af200
vandersoul <111820309+vandersoul@users.noreply.github.com>
2024-04-01 11:14:12 -0300

    up

---
+++ clicou.js
@@ -0,0 +1,8 @@
+let click = 0
+function clicou(){
+    click++
+    alert (`voce clicou em mim ${click}`);
+}
+ function zerarcontador(){
+    contador = 0;
+ }
---
+++ somaNumeros.js
@@ -0,0 +1,9 @@
+function somaNumeros (a, b){
+    let soma = a+b;
+    alert (`A soma dos valores ${a} e ${b} é ${soma}`);
+}
+ let primeiroNumero = parseInt(prompt(`digite o primeiro numero`));
+ let segundoNumero = parseInt(prompt(`digite o segundo numero`));
+ 
+ somaNumeros (primeiroNumero,segundoNumero);
+ 
